fe3c4ae8f0debf788f08244d07402990a28bf76e
Charlotte Morrison <charlottemaemorrison@gmail.com>
2018-09-03 10:33:42 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+/**
+* Simple inventory program for practicing math operations
+*
+* @author <your name here>
+* @version 1.0
+*/
+
+//uncomment each print statement after you have written the math operation.
+public class Main {
+
+	public static void main(String[] args) {
+		int invMilk = 0;
+		int invEggs = 0;
+		int invCheese = 0;
+		
+/* a shipment arrives, add 10 to invMilk, 20 to invEggs, 5 to invCheese*/
+		
+		
+		System.out.println("Step 1:  ");
+		System.out.println("Milk in stock: " + invMilk);
+		System.out.println("Eggs in stock: " + invEggs);
+		System.out.println("Cheese in stock: " + invCheese + "\n");
+
+/* a customer purchases 2 eggs, 1 milk and 1 cheese*/
+		
+		
+
+		//System.out.println("Step 2:  ");
+		//System.out.println("Milk in stock: " + invMilk);
+		//System.out.println("Eggs in stock: " + invEggs);
+		//System.out.println("Cheese in stock: " + invCheese + "\n");
+		
+/* a shipment arrives with 10 eggs for each cheese CURRENTLY in stock */
+		
+		
+/* the clerk buys boxes that hold 6 eggs each, if he puts exactly 6 eggs in each box
+ * how many would be left over (use modulus)		
+ */
+		
+		
+	}
+
+}
